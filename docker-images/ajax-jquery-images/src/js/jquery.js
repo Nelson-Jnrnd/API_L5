d6426768c055916c6042a10a2e906f12bd2f5570
Nelson-Jnrnd <79466777+Nelson-Jnrnd@users.noreply.github.com>
2022-01-18 19:50:04 +0100

    Removed useless function

--- docker-images/ajax-jquery-images/src/js/jquery.js
+++ docker-images/ajax-jquery-images/src/js/jquery.js
@@ -1,14 +1,10 @@
 
 $( "#roll" ).on( "click", function() {
-    function rollDice() {
-        console.log("rolling dices...");
-        $.getJSON( "/dynamic/diceRoller/?roll=1d6", function(result){
-            console.log(result);
-            if(result.length > 0){
-                $("#result").text("you rolled " + result[0]["query"] + " and the result is -> " + result[0]["result"]);
-            }
-            
-        });
-    }
-    rollDice();
+    console.log("rolling dices...");
+    $.getJSON( "/dynamic/diceRoller/?roll=1d6", function(result){
+        console.log(result);
+        if(result.length > 0){
+            $("#result").text("you rolled " + result[0]["query"] + " and the result is -> " + result[0]["result"]);
+        }
+    });
   });
